a46300cc6ae95eee0bd59778347e45f4560cb357
Shandra <shandradev85@gmail.com>
2020-02-04 16:28:04 -0700

    misc java files

---
+++ CalculateTip.java
@@ -0,0 +1,42 @@
+// Shandra Payne
+package calculatetip;
+
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+
+public class CalculateTip
+{
+    
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        double amountOfBill;
+        double tipAmountDesired;
+        double tipAmountInDollars;
+        double totalAmount;
+        DecimalFormat df = new DecimalFormat();
+        
+        // input for getting amountOfBill 
+        System.out.println("Enter the amount of your bill ( ie: 25.00 ) : ");
+        amountOfBill = input.nextDouble();
+       
+        // input for getting amountToTipPercentage (%) & convert to decimal
+        System.out.println("What percentage would you like to tip ( ie: 15) ? ");
+        tipAmountDesired = input.nextDouble() / 100;
+        
+        // calculate the tipAmountInDollars 
+        tipAmountInDollars = tipAmountDesired * amountOfBill;
+        
+        // calculate totalAmount 
+        totalAmount = tipAmountInDollars + amountOfBill;
+        
+        // set the format for printing tipAmountInDollars and totalAmount
+        df.setMinimumFractionDigits(2);
+        
+        // display tipAmount and totalAmount
+        System.out.println("The amount of the tip is: $" + df.format(tipAmountInDollars) +
+                "\n and the total bill is: $" + df.format(totalAmount));
+    }
+    
+}
---
+++ CompoundInterest.java
@@ -0,0 +1,42 @@
+package compoundinterest;
+
+import java.text.DecimalFormat;
+import javax.swing.JOptionPane;
+
+public class CompoundInterest
+{
+
+    public static void main(String[] args)
+    {
+        double accountBalance;
+        double monthlySavingsAmount;
+        double annualInterestRate;
+        double monthlyInterestRate;
+        String dialogInput;
+        int monthsSaved;
+        DecimalFormat df = new DecimalFormat();
+        
+        
+        dialogInput = JOptionPane.showInputDialog("Enter your monthly savings amount: (ie: 100.00");
+        monthlySavingsAmount = Double.parseDouble(dialogInput);
+       
+        dialogInput = JOptionPane.showInputDialog("What is your annual interest rate? (ie: 15)");
+        annualInterestRate = Double.parseDouble(dialogInput);
+        annualInterestRate = annualInterestRate / 100;
+       
+        monthlyInterestRate = annualInterestRate / 12;
+        
+        accountBalance = 0;
+        
+        df.setMaximumFractionDigits(2);
+        
+        for(monthsSaved = 1; monthsSaved < 7 ; monthsSaved++)
+        {
+           accountBalance = (monthlySavingsAmount + accountBalance) * (1 + monthlyInterestRate);
+        }
+         
+        JOptionPane.showMessageDialog(null, "After 6 months your balance will be: $" + df.format(accountBalance));
+     
+    }
+    
+}
