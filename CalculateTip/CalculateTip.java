36ca1e6f532dbaea5c9e9a5ede507a0f562810d5
Shandra <shandradev85@gmail.com>
2020-02-07 18:52:42 -0700

    Update CalculateTip

--- CalculateTip/CalculateTip.java
+++ CalculateTip/CalculateTip.java
@@ -14,29 +14,21 @@ public class CalculateTip
         double tipAmountInDollars;
         double totalAmount;
         DecimalFormat df = new DecimalFormat();
-        
-        // input for getting amountOfBill 
+    
         System.out.println("Enter the amount of your bill ( ie: 25.00 ) : ");
         amountOfBill = input.nextDouble();
        
-        // input for getting amountToTipPercentage (%) & convert to decimal
         System.out.println("What percentage would you like to tip ( ie: 15) ? ");
         tipAmountDesired = input.nextDouble() / 100;
         
-        // calculate the tipAmountInDollars 
         tipAmountInDollars = tipAmountDesired * amountOfBill;
-        
-        // calculate totalAmount 
         totalAmount = tipAmountInDollars + amountOfBill;
-        
-        // set the format for printing tipAmountInDollars and totalAmount
+
         df.setMinimumFractionDigits(2);
-        
-        // display tipAmount and totalAmount
+    
         System.out.println("The amount of the tip is: $" + df.format(tipAmountInDollars) +
                 "\n and the total bill is: $" + df.format(totalAmount));
 
-        // close Scanner
         input.close();
     }
     
