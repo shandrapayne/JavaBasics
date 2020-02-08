add2f8d4fb5355341bcefc8e5bca16aa75d7b3ff
Shandra <shandradev85@gmail.com>
2020-02-07 19:02:56 -0700

    update addition game

--- TheAdditionGame/TheAdditionGame.java
+++ TheAdditionGame/TheAdditionGame.java
@@ -7,35 +7,24 @@ public class TheAdditionGame
     {
         int problemsToAttempt, numOfCorrectAnswers, problemsGiven,
                 rand1, rand2, solution, userAnswer;
-        Scanner input;
+        Scanner input = new Scanner(System.in);
 
-        input = new Scanner(System.in);
-
-        // prompt user for how many problems they want
         System.out.print(" Welcome to The Addition Game! "
                 + "\n Enter the number of problems you would like to solve: ");
         problemsToAttempt = input.nextInt();
-
-        System.out.println("Okay. Let's begin.");
-        
-         // initalize 
+        System.out.println("Okay. Let's begin."); 
         numOfCorrectAnswers = 0;
         problemsGiven = 0;
 
         do
         {
-            // generate two rand integers between 10-50 (inclusive) using Math.random method
             rand1 = 10 + (int) (Math.random() * 41);
             rand2 = 10 + (int) (Math.random() * 41);
-            
-             // solution
+        
             solution = rand1 + rand2;
-
-            // display the problem
             System.out.print(rand1 +  " + " + rand2 + " = ");
             userAnswer = input.nextInt();
 
-            // display if answer is correct or not
             if (userAnswer == solution)
             {
                 System.out.println("That's right!");
@@ -51,7 +40,6 @@ public class TheAdditionGame
 
         } while (problemsGiven < problemsToAttempt);
 
-        // completed all problems
         System.out.println("You answered " + numOfCorrectAnswers + " of " + problemsGiven + " problems correctly.");
         System.out.println("Thanks for playing!");
         System.exit(0);
