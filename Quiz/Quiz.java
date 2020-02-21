85d7e759a7bcac5d0d74c53bc068de95e26acd92
Shandra <shandradev85@gmail.com>
2020-02-21 09:07:39 -0700

    added mini quiz given in class

---
+++ Quiz/Quiz.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Quiz {
+
+    public static void main(String[] args) {
+String s, t;
+      
+        s = repeatIt("abc");
+
+        t = repeatItNtimes("xy", 4);
+
+    }
+
+    public static String repeatIt(String phrase) {
+        System.out.println(phrase);
+        return phrase;
+    }
+
+    public static String repeatItNtimes(String phrase, int numOfTimes) {
+        int x;
+        x=0;
+        while (x <= numOfTimes) {
+            x++;
+            System.out.println(phrase);
+        }
+        return phrase;
+
+    }
+
+}
+/* mini-quiz from class. We were given the just the method names, and told to create the 
+methods. */
