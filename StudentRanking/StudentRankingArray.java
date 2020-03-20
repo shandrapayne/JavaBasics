ed91b6e5e651f194e178c74964c5bd9d057139f6
Shandra <shandradev85@gmail.com>
2020-03-19 19:11:42 -0600

    reformat notes for StudentRanking

--- StudentRanking/StudentRankingArray.java
+++ StudentRanking/StudentRankingArray.java
@@ -76,8 +76,10 @@ public class StudenRanking {
     int starCount;
 
     /*
-     * example: 0.0 to 0.49 (48) ***** 0.5 to 0.99 (82) ******** 1.0 to 1.49 (65)
-     * *******
+     * example: 
+     * 0.0 to 0.49 (48) ***** 
+     * 0.5 to 0.99 (82) ******** 
+     * 1.0 to 1.49 (65) *******
      * 
      * The number in parentheses represents the total number of students that fall
      * within that category. Each * represents 10 students rounded to the nearest
@@ -96,7 +98,9 @@ public class StudenRanking {
      * the student with rank 27. Please output all the students in the order they
      * are in the input file such as: example:
      * 
-     * S316542 3.45 27 S29463 1.42 801 S969870 2.75 T64 with 8 others
+     * S316542 3.45 27 
+     * S29463 1.42 801 
+     * S969870 2.75 T64 with 8 others
      * 
      * Notice that the rank for the last student indicates T64 which means that
      * there are 63 students with a gpa better than this student. It also indicates
